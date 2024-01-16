12e4f90d85d5804a0e32ec81120a18fbc423ed30
Isaac Sheets <iss@bendingspoons.com>
2024-01-16 13:49:53 +0100

    if you can't beat 'em, join 'em

---
+++ portfolio/complete-a-tweet-2000/precache-manifest.7a9b641be6f68f12901fe8a5bce3d13c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2061b9c129dd81b74f466f95c49aa544",
+    "url": "/portfolio/complete-a-tweet-2000/index.html"
+  },
+  {
+    "revision": "4a0844f86134393db9a2",
+    "url": "/portfolio/complete-a-tweet-2000/static/css/2.042331fb.chunk.css"
+  },
+  {
+    "revision": "08d66ad5e184ef64952c",
+    "url": "/portfolio/complete-a-tweet-2000/static/css/main.b36046ea.chunk.css"
+  },
+  {
+    "revision": "4a0844f86134393db9a2",
+    "url": "/portfolio/complete-a-tweet-2000/static/js/2.c6cf22be.chunk.js"
+  },
+  {
+    "revision": "cacc3fb085782228959ac8a3fb7f2a65",
+    "url": "/portfolio/complete-a-tweet-2000/static/js/2.c6cf22be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d66ad5e184ef64952c",
+    "url": "/portfolio/complete-a-tweet-2000/static/js/main.94146cd9.chunk.js"
+  },
+  {
+    "revision": "16476ea9a29b6f9b725f",
+    "url": "/portfolio/complete-a-tweet-2000/static/js/runtime-main.d50c0462.js"
+  },
+  {
+    "revision": "ef6e12417b43eafc91c2b91541558013",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/bad.ef6e1241.mp3"
+  },
+  {
+    "revision": "bcf1565a642a0eb5cb85eecc5f7aa7ca",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/bg.bcf1565a.svg"
+  },
+  {
+    "revision": "ecde8151d6492d37fa9af66fea04cd45",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/click.ecde8151.mp3"
+  },
+  {
+    "revision": "65d5070ed7552056cd39c2784452a77b",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/holes_5.65d5070e.svg"
+  },
+  {
+    "revision": "fa49b61e061282aa9e26029cc59d7391",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/info.fa49b61e.svg"
+  },
+  {
+    "revision": "f8f4853ed700ecb4ff1a179468307b4b",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/no_sound.f8f4853e.svg"
+  },
+  {
+    "revision": "1d3d03ec0cb3e47ab5c0eb185e7991b4",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/print.1d3d03ec.mp3"
+  },
+  {
+    "revision": "0e2f27b86d735a2eda49a2becc0e9140",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/shred.0e2f27b8.mp3"
+  },
+  {
+    "revision": "de43e1cff0e6d4932315fec6c470fa6a",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/sound.de43e1cf.svg"
+  },
+  {
+    "revision": "6ba164156b7560f57b0aa8f84480d962",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/success.6ba16415.mp3"
+  },
+  {
+    "revision": "92cc9868ba8755a8296f5a9c729a2d63",
+    "url": "/portfolio/complete-a-tweet-2000/static/media/type.92cc9868.mp3"
+  }
+]);
